1c636ef9b6f66a55014e82359bfc0bd1d98a49cd
dandanpili <shaoruilin151@qq.com>
2021-02-28 00:22:08 +0800

    实现es的数据的存储以及查询

--- src/main/java/com/shao/controller/ContentController.java
+++ src/main/java/com/shao/controller/ContentController.java
@@ -1,8 +1,37 @@
 package com.shao.controller;
 
+import com.shao.service.ContentService;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
 /**
  * @author shaoruilin
  * @create 2021-02-25-22:33
  */
+
+@RestController
 public class ContentController {
+
+    @Autowired
+    private ContentService contentService;
+
+    @GetMapping("/parse/{keyword}")
+    public Boolean parse(@PathVariable("keyword") String keyword) throws Exception {
+        return contentService.parseContent(keyword);
+    }
+
+    @GetMapping("/search/{keyword}/{pageNo}/{pageSize}")
+    public List<Map<String, Object>> search(@PathVariable("keyword") String keyword,
+                                            @PathVariable("pageNo") Integer pageNo,
+                                            @PathVariable("pageSize") Integer pageSize) throws IOException {
+        return contentService.searchPage(keyword, pageNo, pageSize);
+    }
 }
--- src/main/java/com/shao/service/ContentService.java
+++ src/main/java/com/shao/service/ContentService.java
@@ -1,8 +1,88 @@
 package com.shao.service;
 
+import com.alibaba.fastjson.JSON;
+import com.shao.pojo.Content;
+import com.shao.utils.HtmlParseUtil;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.TermQueryBuilder;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author shaoruilin
  * @create 2021-02-25-22:34
  */
+@Service
 public class ContentService {
+
+    @Autowired
+    private RestHighLevelClient restHighLevelClient;
+
+    @Autowired
+    private HtmlParseUtil htmlParseUtil;
+
+    // 解析数据放入es索引中
+    public Boolean parseContent(String keyword) throws Exception {
+        List<Content> contents = htmlParseUtil.parseJD(keyword);
+        // 把查询到的数据放入es中,批量请求
+        BulkRequest bulkRequest = new BulkRequest();
+        bulkRequest.timeout("2m");
+
+        for (Content content : contents) {
+            bulkRequest.add(
+                    new IndexRequest("jd_goods")
+                            .source(JSON.toJSONString(content), XContentType.JSON));
+        }
+        BulkResponse bulk = restHighLevelClient.bulk(bulkRequest, RequestOptions.DEFAULT);
+        return !bulk.hasFailures();
+    }
+
+    public List<Map<String, Object>> searchPage(String keyword, int pageNo, int pageSize) throws IOException {
+        if (pageNo < 1) {
+            pageNo = 1;
+        }
+
+        // 条件搜索
+        SearchRequest searchRequest = new SearchRequest("jd_goods");
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+
+        // 分页
+        searchSourceBuilder.from(pageNo);
+        searchSourceBuilder.size(pageSize);
+
+        // 精准匹配
+        TermQueryBuilder termQueryBuilder = QueryBuilders.termQuery("title", keyword);
+        searchSourceBuilder.query(termQueryBuilder);
+        searchSourceBuilder.timeout(new TimeValue(60, TimeUnit.SECONDS));
+
+        // 执行搜索
+        searchRequest.source(searchSourceBuilder);
+        SearchResponse searchResponse = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
+
+        // 解析结果
+        ArrayList<Map<String, Object>> list = new ArrayList<>();
+        for (SearchHit documentFields : searchResponse.getHits().getHits()) {
+            list.add(documentFields.getSourceAsMap());
+        }
+        return list;
+    }
 }
